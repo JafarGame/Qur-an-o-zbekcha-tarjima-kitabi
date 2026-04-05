79c4dd0d383123988b0abba12512930ed186021b
JafarGame <muvahhidj@gmail.com>
2026-04-05 23:48:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ const bot = new TelegramBot(token, { polling: true });
 const myTranslation = {
   "1:1": "Mehribon va rahmli Allah nomi bilan.",
   "1:2": "Hamd butun olamlarning Robbi bo‘lgan Allahgadir.",
-  "1:3": "O‘ta Mehribon, O‘ta Rahimli."
+  "1:3": "O‘ta Mehribon, O‘ta Rahimli.",
+  "1:4": "Jazo kunining Egasi.",
+  "1:5": "Faqat Senga ibodat qilamiz va faqat Sendan yordam so‘raymiz.",
+  "1:6": "Bizni to‘g‘ri yo‘lga hidoyat qil.",
+  "1:7": "Ne’mat bergan zotlarning yo‘liga, g‘azabga uchraganlarning va adashganlarning yo‘liga emas."
 };
 const myTafsir = {
   "1:1": "Bu ayat har bir ishni Allah nomi bilan boshlashga undaydi. Unda Allahning rahmati zikr qilingan.",
   "1:2": "Bu ayatda barcha hamd va maqtovlar faqat Allahga tegishli ekani bayon qilinadi. U olamlarning egasidur.",
-  "1:3": "Bu ayatda Allahning cheksiz rahmati yana ta’kidlanadi. U barcha bandalariga mehribon va rahmlidir."
+  "1:3": "Bu ayatda Allahning cheksiz rahmati yana ta’kidlanadi. U barcha bandalariga mehribon va rahmlidir.",
+  "1:4": "Bu ayatda Qiyomat kuni faqat Allahga tegishli ekani va barcha hisob-kitob Uning qo‘lida ekani bayon qilinadi.",
+  "1:5": "Bu oyatda bandaning faqat Allahga ibodat qilishi va faqat Undan yordam so‘rashi kerakligi bayon qilinadi.",
+  "1:6": "Bu oyatda banda Allahdan to‘g‘ri yo‘lni ko‘rsatishni va unda sobit qilishni so‘raydi.",
+  "1:7": "Bu oyatda hidoyat topganlar yo‘li bilan adashganlar va g‘azabga uchraganlar yo‘li farqlanadi."
 };
 // START
 bot.onText(/\/start/, (msg) => {

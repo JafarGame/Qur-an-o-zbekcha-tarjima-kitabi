55dba9ec7d150039781ab70c38a41a4b2ad9f9f9
JafarGame <muvahhidj@gmail.com>
2026-04-05 20:13:27 +0300

    Add Telegram bot with /start command and keyboard
    Implemented a Telegram bot that responds to the /start command and provides a button for accessing the Qur'an.

---
+++ index.js
@@ -0,0 +1,24 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = 8533435265:AAFEgvQPI2TW5YC13thE9XZtbyEPOXctXBU;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, `🕌 Assalomu alaykum!
+
+📖 Qur'an botiga xush kelibsiz
+
+👇 Pastdagi tugmani bosing`, {
+        reply_markup: {
+            keyboard: [["📖 Qur'an kitob"]],
+            resize_keyboard: true
+        }
+    });
+});
+
+bot.on('message', (msg) => {
+    if (msg.text === "📖 Qur'an kitob") {
+        bot.sendMessage(msg.chat.id, "📚 Suralar tez orada chiqadi in shaa Allah");
+    }
+});
